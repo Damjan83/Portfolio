ba706693bc67852796fe723d0e8f66298ce721db
Damjan83 <damjanpokrajac83@gmail.com>
2022-03-16 16:05:26 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,48 @@
-const checkbox = document.getElementById('switchCheckbox');
-
+/*const checkbox = document.getElementById('switchCheckbox');
 const burger = document.querySelector('.main-nav__burger');
 const nav = document.querySelector('.main-nav__menu');
 const menuItem = document.querySelector('.main-nav__menu-item');
-const menuLink = document.querySelectorAll('.main-nav__menu-link');
+const menuLink = document.querySelectorAll('.main-nav__menu-link');*/
 
 /*------Theme switcher------*/ 
-checkbox.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+//checkbox.addEventListener('change', () => {
+//    document.body.classList.toggle('dark');
+//});
+ $(document).ready(() => {
+     $('#switchCheckbox').on('change', () => {
+         $('body').toggleClass('dark');
+     })
+ })
+
+ $(document).ready(() => {
+     $('.main-nav__burger').on('click', () => {
+        $('.main-nav__menu').toggleClass('is-active');
+        $('.main-nav__burger').toggleClass('is-active');
+        $('.main-nav__menu-item').toggleClass('is-active');
+        $('.main-nav__menu-link').each((el) => {
+            toggleClass('is-active');
+            
+        })
+     })
+ })
+
+ $('a[href^=\\#]').on('click', function(e){     
+    e.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
 });
 
+
+
+
 /*------Burger and mobile menu------*/
-burger.addEventListener('click' , () => {
+/*burger.addEventListener('click' , () => {
     nav.classList.toggle('is-active');
     burger.classList.toggle('is-active');
     menuItem.classList.toggle('is-active');
     menuLink.forEach( (el) => {
         el.classList.toggle('is-active');
     });
-});
+});*/
 
     
    
